e12b8d4bd33f512a21ecc02e704c00b7d7d53f55
haggihaggi <hallgrimur.haraldsson@gmail.com>
2025-06-10 15:26:43 -0400

    people tag changes

--- people/models.py
+++ people/models.py
@@ -1,14 +1,13 @@
 from wagtail.models import Page, Locale
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, InlinePanel
 from django.db import models
 from django.shortcuts import render, redirect
 from django.http import Http404
 from django.urls import path
 from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 from wagtail.images.models import Image
-from base.models import Tag  # Import your Tag model
-from modelcluster.fields import ParentalManyToManyField
+from base.models import TaggedItem, Tag
 
 
 class DepartmentIndexPage(Page):
@@ -46,32 +45,18 @@ class PersonPage(RoutablePageMixin, Page):
         related_name='+'
     )
     
-    # Use ParentalManyToManyField instead - this is Wagtail's recommended approach
-    tags = ParentalManyToManyField(
-        Tag,
-        blank=True,
-        help_text="Select tags that apply to this person"
-    )
-
     content_panels = Page.content_panels + [
         FieldPanel('job_title'),
         FieldPanel('bio'),
         FieldPanel('email'),
         FieldPanel('image'),
         FieldPanel('personal_website'),
-        FieldPanel('tags'),  # Default Wagtail widget
+        InlinePanel('tagged_items', label='Tags'),
     ]
 
     parent_page_types = ['people.PeopleIndexPage']
     subpage_types = []
     
-    # Add debugging to see what's happening
-    def save(self, *args, **kwargs):
-        print(f"DEBUG PersonPage.save() called for {self.title}")
-        print(f"DEBUG: Current tags before save: {list(self.tags.all().values_list('name', flat=True))}")
-        super().save(*args, **kwargs)
-        print(f"DEBUG: Current tags after save: {list(self.tags.all().values_list('name', flat=True))}")
-        return self
 
     @route(r'^edit/$', name='edit')
     def edit_view(self, request):
@@ -79,31 +64,28 @@ class PersonPage(RoutablePageMixin, Page):
             raise Http404
         
         if request.method == 'POST':
-            # Update the fields for current page
+            # TEXT
             self.job_title = request.POST.get('job_title', '')
             self.bio = request.POST.get('bio', '')
             self.personal_website = request.POST.get('personal_website', '')
             
-            # Handle tags - get the selected tag IDs and update
-            selected_tag_ids = request.POST.getlist('tags')
-            selected_tag_ids = [int(tag_id) for tag_id in selected_tag_ids if tag_id.isdigit()]
-            print(f"DEBUG: Selected tag IDs from form: {selected_tag_ids}")
-            
             locale_pages = PersonPage.objects.filter(
                 translation_key=self.translation_key
             )
-            
-            # Update tags for all locale versions
+
+            # TAGS
+            selected_tag_ids = request.POST.getlist('tags')
+            selected_tag_ids = [int(tag_id) for tag_id in selected_tag_ids if tag_id.isdigit()]
+            selected_tags = Tag.objects.filter(id__in=selected_tag_ids)
+
             for locale_page in locale_pages:
-                print(f"DEBUG: Setting tags for {locale_page.title}")
-                locale_page.tags.set(selected_tag_ids)
-                # Update other fields too for consistency
-                locale_page.job_title = self.job_title
-                locale_page.bio = self.bio 
-                locale_page.personal_website = self.personal_website
-            
+                locale_page.tagged_items.all().delete()
+                for tag in selected_tags:
+                    locale_page.tagged_items.create(tag=tag)
+
+            # IMAGE
             image_file = request.FILES.get('image')
-            if request.FILES.get('image'):
+            if image_file:
                 new_image = Image.objects.create(
                     title=self.title, # Title of image is title of page
                     file=image_file
@@ -114,16 +96,14 @@ class PersonPage(RoutablePageMixin, Page):
                 # Update image for all locale versions
                 for locale_page in locale_pages:
                     locale_page.image = new_image
-                    locale_page.save()
                 
                 # Delete the old image if it existed
                 if old_image:
                     old_image.delete()
-            else:
-                # Save all locale pages if no image was uploaded
-                for locale_page in locale_pages:
-                    locale_page.save()
             
+            for locale_page in locale_pages:
+                locale_page.save_revision().publish()
+
             return redirect(self.url)
         
         return render(request, 'people/person_edit.html', {'page': self})
